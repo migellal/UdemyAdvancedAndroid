fa65c44784970aa2567c3637c699e84d0425d769
migellal <michalgellert@gmail.com>
2018-05-17 00:36:50 +0200

    just

--- app/src/main/java/pl/kursyandroid/advancedandroid/MainActivity.java
+++ app/src/main/java/pl/kursyandroid/advancedandroid/MainActivity.java
@@ -1,13 +1,44 @@
 package pl.kursyandroid.advancedandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "REACTIVE_JUST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("a", "b", "c", "d", "e")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "subscribe");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.d(TAG, "next: " + s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "complete");
+                    }
+                });
     }
 }
