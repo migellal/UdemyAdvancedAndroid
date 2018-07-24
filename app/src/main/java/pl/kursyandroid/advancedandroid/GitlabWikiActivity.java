dcec5179f6befbde813f617323cfcbfb959f37d1
migellal <michalgellert@gmail.com>
2018-07-24 23:09:46 +0200

    add gitlab wiki page activity

---
+++ app/src/main/java/pl/kursyandroid/advancedandroid/GitlabWikiActivity.java
@@ -0,0 +1,13 @@
+package pl.kursyandroid.advancedandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitlabWikiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gitlab_wiki);
+    }
+}
